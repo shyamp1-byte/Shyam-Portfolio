24b791cfe7c8281ac90b2cd6955fe9368947bed6
Shyam <shyamie123@gmail.com>
2025-03-08 21:46:59 -0500

    adding mouse

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,10 @@ toggleButton.addEventListener('click', () => {
         localStorage.setItem('dark-mode', 'false');
     }
 });
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+});
